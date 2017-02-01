e16d9d3e367f7c35233c1af1fbfd12562ac2ab95
Hexalv <trenhir@gmail.com>
2017-01-31 22:03:02 -0700

    Add files via upload

--- mainTren.js
+++ mainTren.js
@@ -2,8 +2,19 @@
 function main() {
 var tTop=document.getElementById("tTop");
 var tBot=document.getElementById("tBot");
+var ren=document.getElementById("ren");
 
-tBot.width=tTop.style.height;
+setInterval(sized, 10);
 
-console.log(tTop.height);
+function sized() {
+  var windH=window.innerHeight;
+  //set T height/width
+  tTop.setAttribute("style","height:" + windH/12+"px");
+  tBot.setAttribute("style","width:" + windH/10+"px");
+  //set 'ren' size
+  ren.setAttribute("style","font-size:" + windH/5+"px");
+  ren.setAttribute("style","margin-left:" + 1000+"px");
+
+
+  }
 }
