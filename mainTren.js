d4924a862984c72f8939b05391ffe5f71b2b6d8e
Hexalv <trenhir@gmail.com>
2017-02-27 12:53:31 -0700

    Add files via upload

--- mainTren.js
+++ mainTren.js
@@ -9,41 +9,49 @@ function main() {
   
 
   
-  
-  liStart(lines, a);
+  lines.push();
+  liStart();
 
   
 }
 
-
-function liStart(lines, a) {
+//place line start
+function liStart() {
+  
   lines[a]= new Object();
-  lines[a].xPos = 1; 
-  lines[a].xSt = 0;
-  lines[a].yPos = 1;
-  lines[a].ySt = 0;
-  lines[a].inc = 1;
+  lines[a].xPos = Math.floor(Math.random()*100)+1; 
+  lines[a].xSt = Math.floor(Math.random()*100)+1;
+  lines[a].yPos = lines[a].xPos;
+  lines[a].ySt = Math.floor(Math.random()*100)+1;
+  lines[a].inc = (Math.random()*2)+1;
+  lines[a].incy = Math.random()*2-1;
+  
+  console.log(a);
+  
+  setInterval(liInc, 1);
 
-  setInterval(liInc, 10);
 }
-
-function liInc() {
-
+                                                        
+//draw line
+function liInc(comp) {
+  
 
   if (lines[a].xPos<100) {
 
     lines[a].xPos += lines[a].inc;
-    lines[a].yPos += lines[a].inc;
+    lines[a].yPos += lines[a].incy;
     
   var can=document.getElementById("backG");
   var ctx=can.getContext("2d");
 
     ctx.lineWidth=2;    
     ctx.beginPath();
-    ctx.moveTo(0,0);
+    ctx.moveTo(lines[a].xSt,lines[a].ySt);
     ctx.lineTo(lines[a].xPos, lines[a].yPos);
-    ctx.strokeStyle="red";
+    ctx.strokeStyle="green";
 
     ctx.stroke();        
+  } else {
+
   }
 }
